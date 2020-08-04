49666c8b2f241d7e48a6cca3f87f305e137d3ed6
vinayreddy9542 <reddyv399@gmail.com>
2020-08-04 12:04:32 +0530

    Add files via upload

---
+++ curd/src/main/java/com/example/bank/Product.java
@@ -0,0 +1,49 @@
+package com.example.bank;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	private long id;
+	private String name,brand,madein;
+	private float price;
+	public Product() {
+		
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getMadein() {
+		return madein;
+	}
+	public void setMadein(String madein) {
+		this.madein = madein;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+}
---
+++ curd/src/main/java/com/example/bank/ProductService.java
@@ -0,0 +1,28 @@
+package com.example.bank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listall(){
+		return repo.findAll();
+	}
+	
+	public void save(Product product) {
+		repo.save(product);
+	}
+	
+	public Product get(Long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+}
